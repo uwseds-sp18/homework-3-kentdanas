c246e5b5e0290e055fda67ad22270abb1753a410
Kenten Danas <kdanas@uw.edu>
2018-04-16 12:05:34 -0700

    updated homework3 files. cleaned up test file code

--- test_homework3.py
+++ test_homework3.py
@@ -1,5 +1,7 @@
 # Kenten Danas, DATA 515, Homework 3
 
+#Question 2
+
 import unittest
 from homework3 import create_dataframe
 
@@ -10,13 +12,14 @@ class HW3Test(unittest.TestCase):
 		self.assertTrue(len(create_dataframe("class.db"))>10)
 
 	def test_col_names(self):
-		self.assertTrue(len(create_dataframe("class.db").columns)==3 and 
-			'category_id' in create_dataframe("class.db").columns and 
-			'video_id' in create_dataframe("class.db").columns and 
-			'language' in create_dataframe("class.db").columns)
+		df = create_dataframe("class.db")
+		self.assertTrue(set(create_dataframe("class.db").columns) == {'category_id', 'video_id', 'language'})
 
 	def test_key(self):
-		self.assertTrue(len(create_dataframe("class.db")) == create_dataframe("class.db").groupby(['video_id', 'language', 'category_id']).ngroups)
+		df = create_dataframe("class.db")
+		self.assertTrue(len(df) == df.groupby(['video_id', 'language']).ngroups)
+
+		#test if video_id and language are a key. This is expected to fail on class.db since for that data all three columns are needed for a possible key
 
 	def test_path_exception(self):
 		self.assertRaises(ValueError, create_dataframe, "unknown.db")
